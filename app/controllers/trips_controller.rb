6b24eb67fe9c29f9ad66c4ade799c90f6fb8014d
Skyler <skylerpeacock@yahoo.com>
2020-09-12 11:26:51 -0600

    Trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,16 +1,40 @@
 class TripsController < ApplicationController
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
-  def index
+   def index
+    @trip = Trip.all
   end
 
   def show
   end
 
   def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip= Trip.new(trip_params)
+    if @trip.save
+      redirect_to @trip
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @trip.update(trip_params)
+      redirect_to @trip
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    redirect_to trips_path
   end
 
   private
